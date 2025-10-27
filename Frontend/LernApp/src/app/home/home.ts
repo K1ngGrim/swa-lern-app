c6ae58304f8569841e0ccdfca59fc1337cfa95b9
xnurax <jajanau@gmail.com>
2025-10-27 11:09:20 +0100

    Arbeit

--- Frontend/LernApp/src/app/home/home.ts
+++ Frontend/LernApp/src/app/home/home.ts
@@ -1,6 +1,4 @@
 import { Component } from "@angular/core";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { MatCardModule } from "@angular/material/card";
 import { TranslateModule } from "@ngx-translate/core";
@@ -10,8 +8,6 @@ import { RouterModule } from "@angular/router";
   selector: "app-home",
   standalone: true,
   imports: [
-    MatToolbarModule,
-    MatButtonModule,
     MatIconModule,
     MatCardModule,
     TranslateModule,
