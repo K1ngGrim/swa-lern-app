2faf04b742d0509f4f6187ac9ea6fb62022f366e
xnurax <jajanau@gmail.com>
2025-11-01 13:51:40 +0100

    Api gen frontend

---
+++ Frontend/LernApp/generate-api.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# download the latest swagger definition file
+curl -o swagger.json http://localhost:5001/swagger.json
+# remove the existing api folder
+npx rimraf projects/api/src/lib
+# generate the api client
+npx openapi-generator-cli generate --generator-key=lern-app-api
+# delete the swagger definition file
+rm ./swagger.json
